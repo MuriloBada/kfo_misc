35d64e97573a096a0c328fd73cf5b9d3d7f826a2
Murilo Bada <murilomaffiolettibada@hotmail.com>
2022-11-18 20:54:31 -0300

    ajuste em variavel

--- README.md
+++ README.md
@@ -8,6 +8,11 @@ place ```kfo_misc``` in your resources folder and add ```ensure kfo_misc``` in y
 If you don't want one these features simply comment the code.
 
 ### 1. Disable those achievments from online rdr2 from appearing when you join the game
+### 2. Putting Horses Holster
+### 3. Disabling 1 tap Headshots
+
 
 ## Credits
 [Abdulkadir AKTAŞ](https://github.com/abdulkadiraktas)
+Discord: DArkSouL#5690
+Discord: Sinatra#0101
--- client/client.lua
+++ client/client.lua
@@ -6,11 +6,42 @@ Citizen.CreateThread(function()
         if size > 0 then
             for i = 0, size - 1 do
                 local eventAtIndex = GetEventAtIndex(0, i)
-                if eventAtIndex == GeHashKey("EVENT_CHALLENGE_GOAL_COMPLETE") or eventAtIndex == GetHashKey("EVENT_CHALLENGE_REWARD") or eventAtIndex == GetHashKey("EVENT_DAILY_CHALLENGE_STREAK_COMPLETED") then
+                if eventAtIndex == GetHashKey("EVENT_CHALLENGE_GOAL_COMPLETE") or eventAtIndex == GetHashKey("EVENT_CHALLENGE_REWARD") or eventAtIndex == GetHashKey("EVENT_DAILY_CHALLENGE_STREAK_COMPLETED") then
                     Citizen.InvokeNative(0x6035E8FBCA32AC5E)
                 end
             end
         end
     end
+end)
+------------------------------------------------------------------------------------------
+
+-- 2. Put horse Holster
+function NativeFixMeshIssues(ped, categoryHash)
+    Citizen.InvokeNative(0x59BD177A1A48600A, ped, categoryHash)
+end
+
+function NativeUpdatePedVariation(ped)
+    Citizen.InvokeNative(0xCC8CA3E88256E58F, ped, false, true, true, true, false)
+end
+
+function NativeSetPedComponentEnabled(ped, componentHash, immediately, isMp, cat)
+    if cat then
+        NativeFixMeshIssues(ped, cat)
+    end
+    
+    Citizen.InvokeNative(0xD3A7B003ED343FD9, ped, componentHash, immediately, isMp, true)--ApplyShopItemToPed
+    
+    NativeUpdatePedVariation(ped)
+    return
+end
+
+NativeSetPedComponentEnabled(horsePed, 0xF772CED6, true, true, 0xAC106B30)--apply holster on horse
+------------------------------------------------------------------------------------------
+
+-- 3. Disable Headshots
+AddEventHandler('playerSpawned', function()
+    Citizen.CreateThread(function()
+        Citizen.InvokeNative(0xFAEE099C6F890BB8, PlayerPedId(), 128, 0) -- SET_ENTITY_PROOFS
+    end)
 end)
 ------------------------------------------------------------------------------------------
