e31b4c1369ab8ce24a3fe69c6bb48e5fea4f08ca
Murilo Bada <murilomaffiolettibada@hotmail.com>
2022-11-25 19:52:10 -0300

    fxmanifest addition

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,5 +1,7 @@
 fx_version 'adamant'
 games {'rdr3'}
+author 'Murilo Bada a.k.a KIFOO <murilomaffiolettibada@hotmail.com>'
+description 'An Miscellaneous system for nice features'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
 client_scripts {
