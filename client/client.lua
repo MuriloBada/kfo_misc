3ee4a0f2191446457447e298c7610a59ef9d8e31
Murilo Bada <murilomaffiolettibada@hotmail.com>
2023-01-05 08:24:29 -0300

    remove bell

--- client/client.lua
+++ client/client.lua
@@ -6,7 +6,9 @@ Citizen.CreateThread(function()
         if size > 0 then
             for i = 0, size - 1 do
                 local eventAtIndex = GetEventAtIndex(0, i)
-                if eventAtIndex == GetHashKey("EVENT_CHALLENGE_GOAL_COMPLETE") or eventAtIndex == GetHashKey("EVENT_CHALLENGE_REWARD") or eventAtIndex == GetHashKey("EVENT_DAILY_CHALLENGE_STREAK_COMPLETED") then
+                if eventAtIndex == GetHashKey("EVENT_CHALLENGE_GOAL_COMPLETE") 
+                 or eventAtIndex == GetHashKey("EVENT_CHALLENGE_REWARD") 
+                 or eventAtIndex == GetHashKey("EVENT_DAILY_CHALLENGE_STREAK_COMPLETED") then
                     Citizen.InvokeNative(0x6035E8FBCA32AC5E)
                 end
             end
@@ -43,34 +45,5 @@ AddEventHandler('playerSpawned', function()
     Citizen.CreateThread(function()
         Citizen.InvokeNative(0xFAEE099C6F890BB8, PlayerPedId(), 128, 0) -- SET_ENTITY_PROOFS
     end)
-end)
-------------------------------------------------------------------------------------------
-
--- 4. Bell sounds every hour
-Citizen.CreateThread(function()
-    local sleep = true
-    while true do
-        Citizen.Wait(0)
-        local hora = GetClockHours()
-        local minutos = GetClockMinutes()
-        if hora >= 0 and minutos >= 1 and minutos <= 5 then
-            sleep = false
-            local soundSet , streamName = "VAL_SINGLE_TOLL", "CHURCH_BELL_SOUNDS"
-            local timeout = 0
-            while not LoadStream(soundSet , streamName) do
-                Wait(1)
-                timeout = timeout + 1
-                if timeout > 200 then
-                    break
-                end
-            end
-            Citizen.Wait(0)
-            local streamedmusic = Citizen.InvokeNative(0x0556C784FA056628, soundSet, streamName)
-            PlayStreamFromPosition(-230.1,797.9,134.6, streamedmusic) -- valentine
-        end
-        if sleep then
-            Citizen.Wait(2000)
-        end
-    end
 end)
 ------------------------------------------------------------------------------------------
