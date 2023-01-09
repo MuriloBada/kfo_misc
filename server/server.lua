71e781faf196e32539fb7160e003a6f2173cb3e5
Murilo Bada <murilomaffiolettibada@hotmail.com>
2023-01-09 00:34:18 -0300

    Corrigido bússola

--- server/server.lua
+++ server/server.lua
@@ -1,29 +1,28 @@
 local data = {}
-local usedCompass = false
+local CompassEnabled = false
 
 TriggerEvent("redemrp_inventory:getData",function(call)
     data = call
 end)
 
 AddEventHandler("RegisterUsableItem:Compass", function(source)
-    local _source = source
-    usedCompass = true
+    CompassEnabled = not CompassEnabled
 end)
 
 RegisterNetEvent('kfo_misc:checkCompass')
 AddEventHandler('kfo_misc:checkCompass', function()
     local compass = data.checkItem(source, 'Compass')
 
-    if compass > 0 and usedCompass then
-        TriggerClientEvent('kfo_misc:setMinimap', source, 3)
-
-    elseif compass == 0 and usedCompass then
-        usedCompass = false
-        TriggerClientEvent('kfo_misc:setMinimap', source, 0)
-        
+    if compass > 0 then
+        if CompassEnabled then 
+            TriggerClientEvent('kfo_misc:setMinimap', source, 3)
+        else
+            TriggerClientEvent('kfo_misc:setMinimap', source, 0)
+        end
     else
         TriggerClientEvent('kfo_misc:setMinimap', source, 0)
     end
+
 end)
 
 RegisterNetEvent('kfo_misc:handleInventory')
@@ -31,5 +30,5 @@ AddEventHandler('kfo_misc:handleInventory', function()
     TriggerEvent("redemrp_inventory:getData",function(call)
         data = call
     end)
-    usedCompass = false
+    CompassEnabled = false
 end)
